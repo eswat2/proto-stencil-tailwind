48768d0a10290148f0e89da5bc0a1d932ce66822
Richard Hess <eswat2@gmail.com>
2021-08-27 12:32:02 -0700

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,21 @@
 import typescript from '@rollup/plugin-typescript'
-import pkg from './package.json'
 
 export default {
   input: 'src/index.ts',
   plugins: [ typescript() ],
   output: [
-    { format: 'esm', file: pkg.module },
-    { format: 'cjs', file: pkg.main }
+    { format: 'esm', file: 'dist/index.es.js' },
+    { format: 'cjs', file: 'dist/index.js' }
   ],
-  external: [ 'module', 'tailwindcss',  Object.keys(pkg.dependencies || {}) ]
+  external: [
+    'module',
+    'tailwindcss',
+    'path',
+    'postcss',
+    'fs',
+    'debug',
+    'chalk',
+    'magic-string',
+    'acorn-walk'
+  ]
 }
